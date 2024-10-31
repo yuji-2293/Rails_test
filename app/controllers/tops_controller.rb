4a803f2f81c1b5f07f9c4ee1c48a7f5f1e7241a8
inoueyuji <sikogaku2593@gmail.com>
2024-11-01 04:46:55 +0900

    コミット

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,4 @@
 class TopsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
   def index;end
-
-
 end
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,8 @@ set -o errexit
 bundle install
 yarn install
 yarn build # jsファイルをesbuildでバンドルしているため
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-# 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+RAILS_ENV=production rails assets:precompile
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
