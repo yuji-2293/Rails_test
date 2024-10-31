a8353f85cb232d432b02bbe66051b0c23bc69895
inoueyuji <sikogaku2593@gmail.com>
2024-11-01 03:53:50 +0900

    コミット

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,5 @@ yarn install
 yarn build # jsファイルをesbuildでバンドルしているため
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+# 
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
