a8369f4531065981736a17286fcbc3554393f2c2
inoueyuji <sikogaku2593@gmail.com>
2024-11-01 20:36:43 +0900

    コミット

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,6 @@ yarn build # jsファイルをesbuildでバンドルしているため
 
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-RAILS_ENV=production rails assets:precompile
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+# RAILS_ENV=production rails assets:precompile
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 bundle exec rails db:migrate
