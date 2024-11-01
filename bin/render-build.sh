751a0c2387fcb411557d44817b0f48240a68e4bf
inoueyuji <sikogaku2593@gmail.com>
2024-11-01 20:14:15 +0900

    コミット

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,3 +8,4 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 RAILS_ENV=production rails assets:precompile
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:migrate
